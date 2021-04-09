3f20a5433f0145fbe874e01d12c960f0dcb3b116
Samuel Velter Teles <samuelvelter2@gmail.com>
2021-04-09 14:24:49 -0300

    End of section 4

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,26 @@
+using System.Text;
+using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services,IConfiguration _config)
+        {
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
+            });
+            
+            return services;
+        }
+    }
+}
